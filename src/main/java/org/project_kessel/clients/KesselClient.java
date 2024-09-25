b516d1452c22ab60e158db0d765c25480b18e157
Mark McLaughlin <mmclaugh@redhat.com>
2024-09-25 17:49:08 +0100

    Add no-args contructors for abstract constructors to support quarkus client injection.

--- src/main/java/org/project_kessel/clients/KesselClient.java
+++ src/main/java/org/project_kessel/clients/KesselClient.java
@@ -7,6 +7,15 @@ public abstract class KesselClient<A extends AbstractAsyncStub<A>, B extends Abs
     protected A asyncStub;
     protected B blockingStub;
 
+    /***
+     * No args constructor to support synthetic creation of no-args constructor in CDI in impls for normal scope bean
+     * proxying. Supports producers (@Produces) of @ApplicationScoped beans in containers like Quarkus.
+     * (https://github.com/quarkusio/quarkus/issues/22669#issuecomment-1006147659)
+     */
+    protected KesselClient() {
+
+    }
+
     protected KesselClient(A asyncStub, B blockingStub) {
         this.asyncStub = asyncStub;
         this.blockingStub = blockingStub;
